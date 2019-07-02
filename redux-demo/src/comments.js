766ccea392e43195006ce6e99977327350ba4fb5
MichalAW <michal.wantaeuco@gmail.com>
2019-07-02 19:22:04 +0200

    bug

---
+++ redux-demo/src/comments.js
@@ -0,0 +1,31 @@
+import {
+    ADD_COMMENT
+} from './actions.js';
+// example file comments.js
+function comments(state = [], action) {
+    switch(action.type) {
+        case ADD_COMMENT:
+            return [{
+                id: action.id,
+                text: action.text,
+                votes: 0
+            }
+            , ...state];
+        case THUMB_UP_COMMENT:
+            return state.map(comment => {
+                if(comment.id === action.id) {
+                return {...comment, votes: comment.votes + 1}
+                }
+            return comment;
+            }),
+        case THUMB_DOWN_COMMENT:
+            return state.map(comment => {
+                if(comment.id === action.id) {
+                return {...comment, votes: comment.votes + 1}
+                }
+            return comment;
+            });
+        default:
+            return state;
+    }
+}
---
+++ redux-demo/src/users.js
@@ -0,0 +1,4 @@
+// example file users.js
+function users(state = [], action) {
+    return state;
+}
