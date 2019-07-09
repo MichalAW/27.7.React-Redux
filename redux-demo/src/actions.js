48dbbfd15c381d771c518eb32f7cc8ea66b51a6f
MichalAW <michal.wantaeuco@gmail.com>
2019-07-09 13:07:14 +0200

    still same problem

--- redux-demo/src/actions.js
+++ redux-demo/src/actions.js
@@ -5,7 +5,7 @@ export const ADD_COMMENT = 'ADD_COMMENT';
 export const THUMB_UP_COMMENT = 'THUMB_UP_COMMENT';
 export const THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT';
 
-export function addComment(text) {
+export const function addComment(text) {
     return {
         type: ADD_COMMENT,
         id: uuid.v4(),
@@ -13,14 +13,14 @@ export function addComment(text) {
     }
 }
 
-export function thumbUpComment(commentId) {
+export const function thumbUpComment(commentId) {
     return {
         type: THUMB_UP_COMMENT,
         id: commentId
     }
 }
 
-export function thumbDownComment(commentId) {
+export const function thumbDownComment(commentId) {
     return {
         type: THUMB_DOWN_COMMENT,
         id: commentId
